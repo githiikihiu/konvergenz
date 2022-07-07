db57489befb2933a22213851ef22efcba14fce91
francisgithii <githiifrancis@gmail.com>
2022-07-07 23:03:20 +0300

    Update test.php

--- test.php
+++ test.php
@@ -5,7 +5,11 @@
 	$database = "konvergenz";
 	$server = "database-2.ckcysvomqjoq.eu-west-2.rds.amazonaws.com";
 
-	$franc_handle=mysql_connect($server,$user_name,$password);
-$db_found=mysql_select_db($database,$franc_handle);
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle);
 
+    if ($db_found) {
+
+        print ('connected');
+    }
 ?>
