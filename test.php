18209fa75f8c434d6e5107f8a4f44c863d27ffea
francisgithii <githiifrancis@gmail.com>
2022-07-07 22:30:39 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,15 @@
 <?php
 
-  print ('connected');
-    ?>
+    $user_name = "admin";
+	$password = "#R4RY~}mXRe;mt5D";
+	$database = "konvergenz";
+	$server = "database-2.ckcysvomqjoq.eu-west-2.rds.amazonaws.com:3306";
+
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle);
+
+    if ($db_found) {
+
+        print ('connected');
+    }
+?>
