24e7d19e2946473ad2483a9178b4300a092eeb41
francisgithii <githiifrancis@gmail.com>
2022-07-07 22:54:28 +0300

    Update test.php

--- test.php
+++ test.php
@@ -5,5 +5,7 @@
 	$database = "konvergenz";
 	$server = "database-2.ckcysvomqjoq.eu-west-2.rds.amazonaws.com";
 
-	
+	$franc_handle=mysql_connect($server,$user_name,$password);
+$db_found=mysql_select_db($database,$franc_handle);
+
 ?>
