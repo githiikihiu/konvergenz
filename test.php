16c42401fd1dfd8c9a3194308f9fdea4a2bbde7b
francisgithii <githiifrancis@gmail.com>
2022-07-07 22:52:41 +0300

    Update test.php

--- test.php
+++ test.php
@@ -5,8 +5,5 @@
 	$database = "konvergenz";
 	$server = "database-2.ckcysvomqjoq.eu-west-2.rds.amazonaws.com";
 
-	$db_handle = mysql_connect($server, $user_name, $password);
-	$db_found = mysql_select_db($database, $db_handle);
-
-    
+	
 ?>
