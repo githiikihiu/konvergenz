da4a2f37d7dc17dda14c8dfb5864264b54b38444
francisgithii <githiifrancis@gmail.com>
2022-07-07 22:49:54 +0300

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
     $user_name = "admin";
 	$password = "#R4RY~}mXRe;mt5D";
 	$database = "konvergenz";
-	$server = "database-2.ckcysvomqjoq.eu-west-2.rds.amazonaws.com:3306";
+	$server = "database-2.ckcysvomqjoq.eu-west-2.rds.amazonaws.com";
 
 	$db_handle = mysql_connect($server, $user_name, $password);
 	$db_found = mysql_select_db($database, $db_handle);
